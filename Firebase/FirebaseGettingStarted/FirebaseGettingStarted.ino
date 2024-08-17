75f9a83472bf581c232311de9063855ce70a9375
your name <shafiarnob@gmail.com>
2024-08-17 06:52:53 +0600

    firebase getting started

---
+++ Firebase/FirebaseGettingStarted/FirebaseGettingStarted.ino
@@ -0,0 +1,85 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// Insert your Firebase credentials
+// Insert Firebase project API Key
+#define API_KEY ""
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "" 
+
+//Pins
+#define LED_PIN 32
+
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+int count = 0;
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  WiFi.mode(WIFI_STA); //Optional
+  WiFi.begin("Arnob", "kimjongun");
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+   
+  config.api_key = API_KEY; // Assign the api key (required)
+  config.database_url = DATABASE_URL; // Assign the RTDB URL (required) 
+
+  /* Sign up */
+  //The two empty strings denotes annon user sign up
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+  
+  }
+}
